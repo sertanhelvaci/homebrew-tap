188f491beb68f553d34570bc23493695180a3619
Sertan Helvacı <sertanhelvaci@icloud.com>
2026-07-02 22:41:31 +0300

    pathrule 0.1.4

--- Formula/pathrule.rb
+++ Formula/pathrule.rb
@@ -1,20 +1,18 @@
 class Pathrule < Formula
-  desc "Local AI workspace runtime"
-  homepage "https://pathrule.io"
-  url "https://app.pathrule.io/downloads/pathrule-cli-0.0.1-node20-universal.tar.gz"
-  sha256 "9b83055b4a3a012335b43460d9c4fdd1a4613feb2cd08af7b02a11bd4971bcab"
-  license :cannot_represent
+  desc "Path-scoped memories, rules, and skills for AI coding agents"
+  homepage "https://www.pathrule.io"
+  url "https://registry.npmjs.org/@pathrule/cli/-/cli-0.1.4.tgz"
+  sha256 "ee2d247b8b0e66310b21399738e7d55b596a2577e9d73add319db2f3a4b5ea6b"
+  license "Apache-2.0"
+
   depends_on "node"
 
   def install
-    libexec.install Dir["*"]
-    (bin/"pathrule").write <<~EOS
-      #!/bin/bash
-      exec "#{Formula["node"].opt_bin}/node" "#{libexec}/dist/index.js" "$@"
-    EOS
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
-    system bin/"pathrule", "--version"
+    assert_match "0.1.4", shell_output("#{bin}/pathrule --version")
   end
 end
